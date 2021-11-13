b2187802756f851b6da3f3229a50ded5a97b8559
Bossolin <vladyslav.okipnyak@icloud.com>
2021-11-13 14:01:26 +0100

    added remove button

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-const btn = document.querySelector("#submit");
+const submitBtn = document.querySelector("#submit");
 const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
 const read = document.querySelector("#read");
 const bookList = document.querySelector("ul");
+const addBookBtn = document.querySelector(".open-button");
+const closeAddBookBtn = document.querySelector(".close-button");
+const overlay = document.getElementById("overlay");
+const modal = document.querySelector(".modal");
+
+addBookBtn.addEventListener("click", () => {
+  modal.classList.add("active");
+  overlay.classList.add("active");
+});
+
+closeAddBookBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+});
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -27,25 +46,44 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(newBook);
 }
 
-myLibrary.forEach((book) => {
-  console.log(book);
+myLibrary.forEach((book, index) => {
+  console.log(index);
   let li = document.createElement("li");
-  let bookDescrtiption = `${book.title} by ${book.author}, ${book.pages} pages, ${book.read}`;
-  li.innerText = bookDescrtiption;
+  let bookDescrtiption = `${book.title} by ${book.author}, ${book.pages} pages, ${book.read} <button class="remove-btn" data-book-index='${book.title}'>Remove</button>`;
+  li.innerHTML = bookDescrtiption;
   bookList.appendChild(li);
 });
 
-btn.addEventListener("click", (e) => {
+submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   addBookToLibrary(title.value, author.value, +pages.value, read.checked);
 
   let li = document.createElement("li");
-  let bookDescrtiption = `${title.value} by ${author.value}, ${pages.value} pages, ${read.checked}`;
-  li.innerText = bookDescrtiption;
+  let bookDescrtiption = `${title.value} by ${author.value}, ${pages.value} pages, ${read.checked} <button class="remove-btn" data-book-index='${title.value}'>Remove</button>`;
+  li.innerHTML = bookDescrtiption;
   bookList.appendChild(li);
+  removeBook();
 
   title.value = "";
   author.value = "";
   pages.value = "";
   read.checked = false;
+
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
 });
+
+const removeBook = () => {
+  const removeBtns = document.querySelectorAll(".remove-btn");
+  removeBtns.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const node = e.target.dataset.bookIndex;
+      myLibrary = myLibrary.filter((book) => book.title != node);
+      const bookId = document.querySelector(`[data-book-index="${node}"]`);
+
+      bookList.removeChild(bookId.parentNode);
+    });
+  });
+};
+
+removeBook();
